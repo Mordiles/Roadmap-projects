5e3d9608e8ad354c5df48f8e785ee9b99db27ccc
Mordiles <rafifraditya21@gmail.com>
2026-05-14 14:27:07 +0700

    feat: add DatabaseConnection class for database connectivity and remove abstract Task class

---
+++ Backend Projects/Java Backend Projects/TaskTracker/src/DatabaseConnection.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+  private static final String URL = "jdbc:mysql://localhost:3306/task_tracker";
+
+  private static final String USER = "root";
+  private static final String PASSWORD = "";
+
+  public static Connection connect() throws SQLException {
+    return DriverManager.getConnection(
+        URL,
+        USER,
+        PASSWORD);
+  }
+}
